49b6703616e6b63df446eae08b8f7512ab3bdd3c
youneshenniwrites <youneshenniwrites@gmail.com>
2025-04-28 13:35:38 +0100

    add hover effect to button style

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,7 +20,8 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
-        elevated: "bg-white hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]",
+        elevated:
+          "bg-white hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] hover:-translate-x-[4px] hover:-translate-y-[4px]",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
