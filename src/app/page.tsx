673c9273846759dc1b1b4a89760694b1634fc9fc
youneshenniwrites <youneshenniwrites@gmail.com>
2025-04-28 13:34:08 +0100

    add variant to button style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Button variant="elevated">Hello World</Button>
+    </div>
+  );
 }
